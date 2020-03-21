fc732eb17be7b744e822b713e42716a6d1190ba4
Guillaume RICHARD <g.jf.richard@gmail.com>
2020-03-21 21:49:51 +0100

    Petites mises à jours

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,12 @@
-import _ from 'lodash';
+var d3 = require('d3')
+var _ = require('lodash')
 
 let output = _.without([1, 2, 3], 1);
 console.log(content, output);
+
+
+$('#myTab a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
+
--- dist/public/bundle.js
+++ dist/public/bundle.js
@@ -1,6 +1,14 @@
-import _ from 'lodash';
+var d3 = require('d3')
+var _ = require('lodash')
 
 let output = _.without([1, 2, 3], 1);
 console.log(content, output);
+
+
+$('#myTab a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
+
 var L = require('d3')
 
